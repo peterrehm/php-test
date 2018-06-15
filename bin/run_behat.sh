043c5e28e5d5b6b4a54070d772cb7fbb5c5802c7
Peter Rehm <peter.rehm@renvest.de>
2018-06-15 13:14:10 +0200

    Updated vendors and set chrome as default for behat tests

--- bin/run_behat.sh
+++ bin/run_behat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-/usr/local/bin/phantomjs --webdriver=8643 > /dev/null &
+google-chrome --version
+google-chrome --headless --disable-gpu --disable-popup-blocking --remote-debugging-port=9222 https://www.chromestatus.com &
 /vagrant/bin/behat -c '/vagrant/app/config/behat.yml' "$@"
-killall -9 phantomjs
+killall -9 chrome
---
+++ bin/run_behat_phantomjs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/local/bin/phantomjs --webdriver=8643 > /dev/null &
+/vagrant/bin/behat -c '/vagrant/app/config/behat.yml' "$@"
+killall -9 phantomjs
